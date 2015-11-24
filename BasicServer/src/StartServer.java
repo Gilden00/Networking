421e4e698c56b0b6017aea57070022ca892cc6fc
Gilden00 <connor.r.mcneely@gmail.com>
2015-11-24 15:52:57 -0600

    Added: 	Listens for connection of a client. 	Requests username. 	Prints user input to the console. 		If input is "/exit", the server shuts down.

--- BasicServer/src/StartServer.java
+++ BasicServer/src/StartServer.java
@@ -1,5 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class StartServer {
+	
+	private final static int port = 4444;
+	
 	public static void main(){
+		try {
+			ServerSocket sSocket = new ServerSocket(port);
+			Socket socket = sSocket.accept();
+			
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			
+			out.print("Username: ");
+			String user = in.readLine();
+			
+			boolean exit = true;
+			while(exit){
+				String line = in.readLine();
+				if(line.equals("/quit")){
+					System.out.println(user + " has disconnected.");
+				}else{
+					System.out.println(user + ": " + line);
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
