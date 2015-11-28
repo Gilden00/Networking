b9ca861ae0f3771b4706946e8c8743b0972382c7
Gilden00 <connor.r.mcneely@gmail.com>
2015-11-27 21:49:11 -0600

    Modified Server code.

--- BasicServer/src/StartServer.java
+++ BasicServer/src/StartServer.java
@@ -9,16 +9,33 @@ public class StartServer {
 	
 	private final static int port = 4444;
 	
-	public static void main(){
+	public static void main(String[]args){
 		try {
+			System.out.println("Starting Server...");
 			ServerSocket sSocket = new ServerSocket(port);
+			System.out.println("Server Started. Listening for client.");
 			Socket socket = sSocket.accept();
+			System.out.println("Client found.");
 			
+			System.out.println("Preparing.");
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			System.out.println("Ready. Waiting for confirmation.");
 			
-			out.print("Username: ");
-			String user = in.readLine();
+			while(!in.ready()){}
+			
+			System.out.println("Confirmed.");
+			
+			out.println("Username: ");
+			String user = "<noname>";
+			while(user.equals("<noname>")){
+				String line = in.readLine();
+				if(!line.equals("/Ready")){
+					user = line;
+				}
+			}
+			
+			System.out.println(user + " has connected.");
 			
 			boolean exit = true;
 			while(exit){
@@ -29,6 +46,9 @@ public class StartServer {
 					System.out.println(user + ": " + line);
 				}
 			}
+			
+			socket.close();
+			sSocket.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
