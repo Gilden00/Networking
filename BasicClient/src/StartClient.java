c87c6d899a7cb2383340f43af9a8ad882a7b3c3f
Gilden00 <connor.r.mcneely@gmail.com>
2015-11-27 21:48:26 -0600

    Wrote client code

--- BasicClient/src/StartClient.java
+++ BasicClient/src/StartClient.java
@@ -1,4 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class StartClient {
-
+	
+	private static final String hostname = "127.0.0.1";
+	private static final int port = 4444;
+	
+	public static void main(String[]args){
+		try {
+			Socket socket = new Socket(hostname, port);
+			
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			
+			Scanner scan = new Scanner(System.in);
+			
+			while(!in.ready()){
+				out.println("/Ready");
+			}
+			
+			System.out.print(in.readLine());
+			String line = scan.nextLine();
+			out.println(line);
+			
+			boolean exit = true;
+			while(exit){
+				if(in.ready()){
+					System.out.println(in.readLine());
+				}
+				if(scan.hasNextLine()){
+					out.println(scan.nextLine());
+				}
+			}
+			scan.close();
+			socket.close();
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
